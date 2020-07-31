cbeb2fe68ca5f6c1621468c7e2826b2d3389f41f
trungthn21 <68836049+trungthn21@users.noreply.github.com>
2020-07-31 18:39:04 +0700

    finish product-detail

--- resources/views/item-list.blade.php
+++ resources/views/item-list.blade.php
@@ -118,19 +118,133 @@
       <div class="related-product-text">
           <div class="text">
             <strong>Related Product </strong>
-          </div>  
+          </div>
+          <div class="arrow">
+            <div class="arrow-left">
+              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
 
+            </div>
+            <div class="arrow-right">
+              <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+            </div>
+        </div>
+        <br>
+        <br>
+        <div class="product-image-wrapper">
+        <div class="product-image">
+          <img src="image/vertical rectangle.jpg" alt="">
+        <div class="product-info">
+          <div>
+          <span><strong>Crown Summit Backpack </strong> </span>
+          </div>
+          <div>
+          <span style="color:lightGreen"> $250.00 </span>
+        </div>
+        </div>
+        </div>
+        </div>
 
+      <div class="product-image-wrapper">
+      <div class="product-image">
+        <img src="image/vertical rectangle.jpg" alt="">
+      <div class="product-info">
+        <div>
+        <span><strong>Crown Summit Backpack </strong> </span>
+        </div>
+        <div>
+        <span style="color:lightGreen"> $250.00 </span>
+      </div>
+      </div>
+      </div>
+      </div>
 
+  <div class="product-image-wrapper">
+  <div class="product-image">
+    <img src="image/vertical rectangle.jpg" alt="">
+  <div class="product-info">
+    <div>
+    <span><strong>Crown Summit Backpack </strong> </span>
+    </div>
+    <div>
+    <span style="color:lightGreen"> $250.00 </span>
+  </div>
+  </div>
+  </div>
+  </div>
 
+<div class="product-image-wrapper">
+<div class="product-image">
+  <img src="image/vertical rectangle.jpg" alt="">
+<div class="product-info">
+  <div>
+  <span><strong>Crown Summit Backpack </strong> </span>
 
+  </div>
+  <div>
+  <span style="color:lightGreen"> $250.00 </span>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
 
+<div class="footer">
+<div class="connect">
+  <hr>
 
+  <div class="connect-left">
+    We deliver your dream Website.Stay connect with us
+  </div>
+  <div class="connect-right">
+    <i class="fab fa-twitter fa-lg" style="margin-right:10px" ></i>
+    <i class="fab fa-facebook fa-lg" style="margin-right:10px"></i>
+    <i class="fab fa-instagram fa-lg"style="margin-right:10px"></i>
+    <i class="fab fa-linkedin fa-lg"style="margin-right:10px"></i>
 
+  </div>
+  <hr style="color:lightgrey; ">
 
+</div>
 
 
 </div>
+
+<div class="info-page">
+<ul>
+  <li><h4> Information </h4></li>
+ <li><a href='' title='Link'>Delivery Information</a></li>
+ <li><a href='' title='Link'>Site Map</a></li>
+ <li><a href='' title='Link'>Contact Us</a></li>
+ <li><a href='' title='Link'>Privecy Policy</a></li>
+ <li><a href='' title='Link'>My Account</a></li>
+
+
+</ul>
+<ul>
+  <li><h4> Your Account </h4></li>
+ <li><a href='' title='Link'>Track YOur Order</a></li>
+ <li><a href='' title='Link'>My Wishlist</a></li>
+ <li><a href='' title='Link'>My Account</a></li>
+</ul>
+</ul>
+<ul>
+  <li><h4> Help </h4></li>
+ <li><a href='' title='Link'>FAQ</a></li>
+ <li><a href='' title='Link'>Shipping</a></li>
+ <li><a href='' title='Link'>Privacy Policy</a></li>
+</ul>
+<ul>
+  <li><h4> Contact Information </h4></li>
+ <li><a href='' title='Link'>123 Your Address</a></li>
+ <li><a href='' title='Link'>+121231231241</a></li>
+ <li><a href='' title='Link'><span style="color=lightGreen">email@emailaddress.com</a><span></li>
+</ul>
+</div>
+
+</div>
+
+
 <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/owl.carousel.min.js"></script>
 <script src="js/bootstrap-input-spinner-master/src/bootstrap-input-spinner.js"></script>
