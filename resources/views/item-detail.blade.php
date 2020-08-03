0f17cc38ce8ff642845ac5c648886cf6a5afeadd
trungthn21 <68836049+trungthn21@users.noreply.github.com>
2020-08-03 16:24:21 +0700

    update product-detail page

--- resources/views/item-detail.blade.php
+++ resources/views/item-detail.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Product-page</title>
     <link rel="stylesheet" type="text/css" href="css/product-css.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <script src="https://kit.fontawesome.com/ac206ac257.js" crossorigin="anonymous"></script>
 
 
@@ -12,7 +13,7 @@
   <body>
     <div class="header" >
       <div class ="header-left">
-        <a href="homepage.blade.php" class="homepage"><span style="color: lightgreen; font-size=px">Renoshop</span>Bee</a>
+        <a href="homepage.blade.php" class="homepage"><span style="color: lightgreen; font-size:30px">Renoshop</span>Bee</a>
       </div>
       <div class="header-center">
         <a class="active" href="homepage.blade.php">Home</a>
@@ -41,64 +42,72 @@
 
     <div class="product-info">
         <div class="product-info-image">
+          <div>
             <img src="image/vertical rectangle.jpg" >
+          </div>
+        <div id="sync1" class="owl-carousel">
+          <div class="item">
+            <img src="image/vertical rectangle.jpg" alt="">
+          </div>
+          <div class="item">
+            <img src="image/vertical rectangle.jpg" alt="">
+          </div>
+          <div class="item">
+            <img src="image/vertical rectangle.jpg" alt="">
+          </div>
         </div>
-        <div class="product-info-text">
-          <h2>This Is The Product Name </h2>
-          <span style="color: lightGreen; margin-left:5px; font-size: 22px"> $ 250.00         </span>
-          <span>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star" aria-hidden="true"></i>
-            <i class="fa fa-star-o" aria-hidden="true"></i>
-          </span>
-          <br>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at ultricies arcu, eu hendrerit est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet diam ac dolor ornare lobortis. Integer vel rhoncus ex. Ut posuere luctus mollis. </p>
 
-          <hr style="color: lightgrey">
-          <br>
-          <select name="size"  form="orderform" style="width: 48%;float:left;height:25px;margin-bottom:25px">
-            <option value ="Size"> Choose your size </option>
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="opel">Opel</option>
-            <option value="audi">Audi</option>
-          </select>
+        
 
-          <select name="size"  form="orderform" style="width: 48%;float:right;height:25px; margin-bottom:25px">
-            <option value ="Size"> Color </option>
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="opel">Opel</option>
-            <option value="audi">Audi</option>
-          </select>
-          <input  type="number"  value="1" min="1" max="100" step="1"/>
-          <br>
-          <div class="cart-container">
-            <div class="cart">
-              <i class="fa fa-shopping-cart style" aria-hidden="true"></i> Add to Cart
-            </div>
-            <div class="icon">
-              <i class="fa fa-heart-o" aria-hidden="true" style="border:1px solid grey; padding:10px"></i>
-              <i class="fa fa-heart-o" aria-hidden="true"style="border:1px solid grey;padding:10px"></i>
-            </div>
+      </div>
+
+
+
+      <div class="product-info-text">
+        <h2>This Is The Product Name </h2>
+        <span style="color: lightGreen; margin-left:5px; font-size: 22px"> $ 250.00         </span>
+        <span>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star" aria-hidden="true"></i>
+          <i class="fa fa-star-o" aria-hidden="true"></i>
+        </span>
+        <br>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam at ultricies arcu, eu hendrerit est. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse sit amet diam ac dolor ornare lobortis. Integer vel rhoncus ex. Ut posuere luctus mollis. </p>
+
+        <hr style="color: lightgrey">
+        <br>
+        <select name="size"  form="orderform" style="width: 48%;float:left;height:25px;margin-bottom:25px">
+          <option value ="Size"> Choose your size </option>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+
+        <select name="size"  form="orderform" style="width: 48%;float:right;height:25px; margin-bottom:25px">
+          <option value ="Size"> Color </option>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+        <input  type="number"  value="1" min="1" max="100" step="1"/>
+        <br>
+        <div class="cart-container clearfix">
+          <div class="cart">
+            <i class="fa fa-shopping-cart style" aria-hidden="true"></i> Add to Cart
+          </div>
+          <div class="icon">
+            <i class="fa fa-heart-o" aria-hidden="true" style="border:1px solid grey; padding:10px"></i>
+            <i class="fa fa-heart-o" aria-hidden="true"style="border:1px solid grey;padding:10px"></i>
           </div>
         </div>
       </div>
+    </div>
 
-      <div class="thumbnail-carousel">
-        <div class="item">
-          <img src="image/vertical rectangle.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="image/vertical rectangle.jpg" alt="">
-        </div>
-        <div class="item">
-          <img src="image/vertical rectangle.jpg" alt="">
-        </div>
-      </div>
-
+      <div style="clear:both"></div>
 
       <div class="review-description">
         <div class="title">
@@ -114,6 +123,7 @@
           Pellentesque ultricies elit id ullamcorper interdum. Vestibulum varius augue non turpis viverra tempus. Donec at congue massa, vitae tempor nibh. Nulla venenatis congue turpis, a condimentum est aliquam ac. Quisque non metus mauris. Donec sollicitudin convallis ornare. Morbi volutpat semper sapien. Suspendisse lacus lectus, rutrum non vulputate et, fringilla eget lorem. Nunc congue vestibulum arcu et sodales. Suspendisse dictum finibus dignissim. Nunc sodales ipsum est, id tempus nisl vulputate nec. Ut non libero ultrices, pulvinar nisl eu, rhoncus quam.
         </div>
       </div>
+      <div style="clear:both"></div>
 
       <div class="related-product-text">
           <div class="text">
@@ -187,8 +197,7 @@
 </div>
 </div>
 </div>
-</div>
-
+<div style="clear:both"></div>
 <div class="footer">
 <div class="connect">
   <hr>
@@ -211,7 +220,7 @@
 </div>
 
 <div class="info-page">
-<ul>
+<ul style="margin-left:0px;">
   <li><h4> Information </h4></li>
  <li><a href='' title='Link'>Delivery Information</a></li>
  <li><a href='' title='Link'>Site Map</a></li>
@@ -243,10 +252,14 @@
 </div>
 
 </div>
+</div>
+
+
+</div>
 
 
 <script src="http://code.jquery.com/jquery-3.2.1.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/owl.carousel.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
 <script src="js/bootstrap-input-spinner-master/src/bootstrap-input-spinner.js"></script>
 
 <script>
@@ -254,7 +267,85 @@ $("input[type='number']").inputSpinner();
 </script>
 
 <script>
+$(document).ready(function() {
 
+    var sync1 = $("#sync1");
+    var sync2 = $("#sync1");
+    var slidesPerPage = 4; //globaly define number of elements per page
+    var syncedSecondary = true;
+
+    sync1.owlCarousel({
+        items: 3,
+        slideSpeed: 2000,
+        nav: true,
+        autoplay: false,
+        dots: true,
+        loop: true,
+        responsiveRefreshRate: 200,
+        navText:false
+    }).on('changed.owl.carousel', syncPosition);
+
+    sync2
+        .on('initialized.owl.carousel', function() {
+            sync2.find(".owl-item").eq(0).addClass("current");
+        })
+        .owlCarousel({
+            items: slidesPerPage,
+            dots: true,
+            nav: true,
+            smartSpeed: 200,
+            slideSpeed: 500,
+            slideBy: slidesPerPage, //alternatively you can slide by 1, this way the active slide will stick to the first item in the second carousel
+            responsiveRefreshRate: 100
+        }).on('changed.owl.carousel', syncPosition2);
+
+    function syncPosition(el) {
+        //if you set loop to false, you have to restore this next line
+        //var current = el.item.index;
+
+        //if you disable loop you have to comment this block
+        var count = el.item.count - 1;
+        var current = Math.round(el.item.index - (el.item.count / 2) - .5);
+
+        if (current < 0) {
+            current = count;
+        }
+        if (current > count) {
+            current = 0;
+        }
+
+        //end block
+
+        sync2
+            .find(".owl-item")
+            .removeClass("current")
+            .eq(current)
+            .addClass("current");
+        var onscreen = sync2.find('.owl-item.active').length - 1;
+        var start = sync2.find('.owl-item.active').first().index();
+        var end = sync2.find('.owl-item.active').last().index();
+
+        if (current > end) {
+            sync2.data('owl.carousel').to(current, 100, true);
+        }
+        if (current < start) {
+            sync2.data('owl.carousel').to(current - onscreen, 100, true);
+        }
+    }
+
+    function syncPosition2(el) {
+        if (syncedSecondary) {
+            var number = el.item.index;
+            sync1.data('owl.carousel').to(number, 100, true);
+        }
+    }
+
+    sync2.on("click", ".owl-item", function(e) {
+        e.preventDefault();
+        var number = $(this).index();
+        sync1.data('owl.carousel').to(number, 300, true);
+    });
+});
 
 </script>
 
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('checkout-page');
+    return view('item-detail');
 });
